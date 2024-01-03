37125fffd0078bd02dd0dcb5940da40a5348673d
Aviral Katiyar <aviralofficial1729@gmail.com>
2024-01-04 04:07:10 +0530

    Update settings.py

--- humorbackend/settings.py
+++ humorbackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_)=&^4pan9q-w*1eqodnzxk(*f_08l$$6hoh+ho#g8*%9mg05+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
